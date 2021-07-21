91422ecb6326a84e4e99df89645fa9711b94876a
Arun Das <arun@Aruns-MacBook-Air.local>
2021-07-21 19:06:27 -0400

    Added icon, starting to put front page together

--- src/App.js
+++ src/App.js
@@ -6,10 +6,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SetKeyCounterButton id="setKey"/>
         <CounterButton id="varKey"/>
       </header>
-      <CounterButton/>
+      <div className="title-and-icon">
+        <title>Welcome to Project Purple Cow</title>
+        <img src="/icon.jpeg" />
+      </div>
+      <div className="button-display">
+        <div className="zero-start">
+          <p>Hit the button to increase the count, starting from 0</p>
+          <CounterButton />
+        </div>
+        <div className="mid-start">
+          <p>Hit the button to increase the count, starting from where the set key left off</p>
+          <SetKeyCounterButton />
+        </div>
+      </div>
     </div>
   );
 }
