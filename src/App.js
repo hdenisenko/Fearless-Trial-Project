34677cd04e1a34130d0af9f891dd714cadc0160e
Arun Das <arun@Aruns-MacBook-Air.local>
2021-07-21 20:08:06 -0400

    Final touches on home page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import SetKeyCounterButton from './SetKeyCounterButton.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <CounterButton id="varKey"/>
-      </header>
+      <header className="App-header" />
       <div className="title-and-icon">
         <title>Welcome to Project Purple Cow</title>
         <img src="/icon.jpeg" />
@@ -21,9 +19,13 @@ function App() {
           <p>Hit the button to increase the count, starting from where the set key left off</p>
           <SetKeyCounterButton />
         </div>
+        <div className="zero-start">
+          <p>Hit the button to increase the count, starting from 0</p>
+          <CounterButton />
+        </div>
       </div>
       <div className="our-mission">
-        <subtitle>Our Misson</subtitle>
+        <title>Our Misson</title>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </div>
